61f1e28f922e02537212688b631cbc766a8f3de8
Regalk <jsalazarcaviedes30@gmail.com>
2023-03-05 17:14:17 -0500

    Implement trait to strings

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
+#[allow(dead_code)]
 pub mod color {
     pub struct Color {
         name: String,
         code: String,
     }
-
 
     impl Color {
         pub fn new(name: &str, code: i32) -> Self {
@@ -14,17 +14,20 @@ pub mod color {
         }
     }
 
-    pub struct Colored;
-    impl Colored {
-        pub fn text_to_color(text: &str, color: Color) -> String {
-            let print = "\u{001b}[38;5;".to_owned() + &color.code +"m "+ &text;
+    pub trait Colored { 
+        fn text_to_color(self, color: Color) -> String;
+        fn text_to_color_8colors(self, color: Color) -> String; 
+    }
+
+    impl Colored for String {
+        fn text_to_color(self, color: Color) -> String {
+            let print = "\u{001b}[38;5;".to_owned() + &color.code +"m "+ &self;
             print.to_string()
         }
 
-        pub fn text_to_color_8colors(text: &str, color: Color) -> String {
-            let print = "\u{001b}[".to_owned() + &color.code +";1m "+ &text;
+        fn text_to_color_8colors(self, color: Color) -> String {
+            let print = "\u{001b}[".to_owned() + &color.code +";1m "+ &self;
             print.to_string()
         }
     }
-
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use coloring_terminal::color::{Color, Colored};
 
 fn main() {
     let color_random = Color::new("random", 31);
-    println!("{}", Colored::text_to_color_8colors("Test", color_random));
+    println!("{}", String::from("Test").text_to_color_8colors(color_random));
 }
