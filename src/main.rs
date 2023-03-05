21db566e08e84eda86a18ad5a9238313351d4d56
Regalk <jsalazarcaviedes30@gmail.com>
2023-03-05 16:37:22 -0500

    Creating the color and coloring struct

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,30 @@
-fn colored(a: i32, b: i32, text: String) -> String { 
-    let code = (a * 16 + b).to_string();
-    let print = "\u{001b}[38;5;".to_owned() + &code +"m "+ &text;
-    format!("{}", print)
+#[derive(Clone)]
+struct Color {
+    name: String,
+    code: String,
 }
 
 
-fn main() {
-    println!("{}", colored(13, 16, "test".to_string()));
-    let color = "\u{001b}[31m A".to_string();
-    println!("{}", color); 
+impl Color {
+    fn new(name: &str, a: i32, b: i32) -> Self {
+        return Self {
+            name: name.to_string(),
+            code: (a * 16 + b).to_string(),
+        }
+    }
+}
 
+
+struct Colored;
+
+impl Colored {
+    fn text_to_color(text: &str, color: Color) -> String {
+        let print = "\u{001b}[38;5;".to_owned() + &color.code +"m "+ &text;
+        return format!("{}", print);
+    }
+}
+
+fn main() {
+    let color_red = Color::new("red", 13, 16);
+    println!("{}, Using the color: {} ", Colored::text_to_color("Regius", color_red.clone()), color_red.name); 
 }
