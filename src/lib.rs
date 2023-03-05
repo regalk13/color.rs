c02fc17789e30eb0f85965c8e4315f04cd4e16bd
Regalk <jsalazarcaviedes30@gmail.com>
2023-03-05 16:52:42 -0500

    Adding option to 8bit colors

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
-
 pub mod color {
-    #[derive(Clone)]
     pub struct Color {
         name: String,
         code: String,
@@ -20,6 +18,11 @@ pub mod color {
     impl Colored {
         pub fn text_to_color(text: &str, color: Color) -> String {
             let print = "\u{001b}[38;5;".to_owned() + &color.code +"m "+ &text;
+            print.to_string()
+        }
+
+        pub fn text_to_color_8colors(text: &str, color: Color) -> String {
+            let print = "\u{001b}[".to_owned() + &color.code +";1m "+ &text;
             print.to_string()
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use coloring_terminal::color::{Color, Colored};
 
 fn main() {
-    let color_random = Color::new("random", 24);
-    println!("{}", Colored::text_to_color("Test", color_random));
+    let color_random = Color::new("random", 31);
+    println!("{}", Colored::text_to_color_8colors("Test", color_random));
 }
